2a9625a8efb2e6e529b3e051312263e756b6ae84
tawny francis <tawnyfrancis@gmail.com>
2019-10-25 11:10:35 -0400

    psudocoded the index.html and worked on creating some buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var trivia = {
+    questions:['q1','q2'],
+    choices:[
+              ['a1','b1','c1'],
+              ['a2','b2','c2']
+            ],
+    answer:['a1','a2']
+  }
+  console.log(trivia.question[questionIndex]);
+  for(var i=0;trivia.choices[questionIndex].length; i++);
